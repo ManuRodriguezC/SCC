f6bc9e32f19495d64325504bc0dca396b2ddc01d
ManuRodriguezC <estebanclimb@gmail.com>
2024-05-17 17:15:26 -0500

    Complate forms date users, created dialog for simulator

--- simulador/views.py
+++ simulador/views.py
@@ -5,10 +5,49 @@ from .models import DeudaAporte, Extracupo
 def simulador(request):
     deudaAporte = DeudaAporte.objects.all()
     extracupo = Extracupo.objects.all()
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        lastname = request.POST.get('lastname')
+        document = request.POST.get('document')
+        salario = request.POST.get('salario')
+        others = request.POST.get('others')
+        debit = request.POST.get('debit')
+        years = request.POST.get('years')
+        score = request.POST.get('score')
+        typecredit = request.POST.get('typecredit')
+        monto = request.POST.get('monto')
+        cuotas = request.POST.get('cuotas')
+        # if str(typecredit) == str(deudaAporte['name']):
+        #     tasa = deudaAporte['tasa']
+        # else:
+        tasa = 0
+        
+        datas = {
+            'name': name,
+            'lastname': lastname,
+            'document': document,
+            'salario': salario,
+            'others': others,
+            'debit': debit,
+            'years': years,
+            'score': score,
+            'typecredit': typecredit,
+            'monto': monto,
+            'cuotas': cuotas,
+            'tasa': tasa,
+            'deudaaporte': deudaAporte,
+            'extracupo': extracupo,
+            'state': True
+        }
+        
+    
+        return render(request, 'simulador.html', {
+            'datas': datas
+        })
     return render(request, 'simulador.html', {
-        'deudaAporte': deudaAporte,
-        'extracupo': extracupo
-    })
+            'deudaaporte': deudaAporte,
+            'extracupo': extracupo})
+
 
 def User(request):
     if request.method == 'POST':
